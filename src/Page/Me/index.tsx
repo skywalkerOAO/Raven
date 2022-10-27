66a68d4fd18701283ae9604d19fccdd1956dbc31
Hexar <wangzhao000130@163.com>
2022-10-27 14:18:10 +0800

    add function

--- src/Page/Me/index.tsx
+++ src/Page/Me/index.tsx
@@ -1,43 +1,54 @@
-import { Button, Card, Typography,Modal } from '@douyinfe/semi-ui'
+import { Button, Card, Typography, Modal, Avatar, Row, Col, Space, Skeleton } from '@douyinfe/semi-ui'
+import Meta from '@douyinfe/semi-ui/lib/es/card/meta'
 import React from 'react'
-import './index.scss'
+import styles from './index.module.scss'
+import Friend from './friend'
 interface IProps {
-  // keys: number
+    // keys: number
 }
 
 interface IState {
-  // key: number
+    signin: boolean
+    user_name: string
+    loading: boolean
 }
 const { Text } = Typography
+const { Title, Paragraph, Image } = Skeleton
 class Me extends React.Component<IProps, IState> {
-  state = {
-    key: 0,
-    visible:false
-  }
-  componentDidMount() {}
-  showDialog = ()=>{
-    this.setState({
-      visible:true
-    })
-  }
-  render() {
-    return (
-      <>
-         <Button onClick={this.showDialog}>打开弹窗123</Button>
-            <Modal
-                title="123456"
-                visible={this.state.visible}
-                onOk={()=>{this.setState({visible:false})}}
-                onCancel={()=>{this.setState({visible:false})}}
-                closeOnEsc={true}
-            >
-                This is the content of a basic modal.
-                <br />
-                More content...
-            </Modal>
-      </>
-    )
-  }
+    state = {
+        signin: false,
+        loading: true,
+        user_name: '请登录'
+    }
+    componentDidMount() {}
+
+    render() {
+        return (
+            <>
+                <Space vertical style={{ width: '100%' }} spacing={'loose'}>
+                    <Card title="个人信息" style={{ width: '100%' }}>
+                        您还没有登录
+                    </Card>
+                    <Card title="个人动态" style={{ width: '100%' }} loading={this.state.loading}>
+                        您还没有登录
+                    </Card>
+                    <Card title="朋友动态" style={{ width: '100%' }}>
+                        <Row>
+                            <Col span={8} className={styles.main_col}>
+                                <Friend loading={this.state.loading} />
+                            </Col>
+                            <Col span={8} className={styles.main_col}>
+                                <Friend loading={this.state.loading} />
+                            </Col>
+                            <Col span={8} className={styles.main_col}>
+                                <Friend loading={this.state.loading} />
+                            </Col>
+                        </Row>
+                    </Card>
+                </Space>
+            </>
+        )
+    }
 }
 
 export default Me
