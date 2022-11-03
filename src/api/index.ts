444f2a700f4e01d6483b4859cd5ea571ed684ed8
Hexar <wangzhao000130@163.com>
2022-11-03 11:51:35 +0800

    no message

---
+++ src/api/index.ts
@@ -0,0 +1,6 @@
+const api_Path = {
+    top_week_services: '/novel/novelHot/top/man/top/hot/week/1.html',
+    weibo_hot_services: '/wbHot/api/v1/weiboHot',
+    zhihu_hot_services:'/zhHot/api/v4/search/top_search/tabs/hot/items'
+  }
+  export default api_Path
