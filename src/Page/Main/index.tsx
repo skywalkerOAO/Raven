09b0e038b8abf9d81691df36450d8d37f589d5cb
Hexar <wangzhao000130@163.com>
2022-11-03 13:57:02 +0800

    bug fix

--- src/Page/Main/index.tsx
+++ src/Page/Main/index.tsx
@@ -11,8 +11,14 @@ interface IState {
     zhihu: Array<any>
 }
 const cardStl = {
-    maxWidth: 500,
-    minWidth: 400
+    minWidth: 480
+}
+interface Iitem {
+    rank: string
+    content: string
+    item: string
+    key: string
+    href: string
 }
 class Main extends React.Component<IProps, IState> {
     state = {
@@ -22,29 +28,29 @@ class Main extends React.Component<IProps, IState> {
 
     async componentDidMount() {
         await store.GetHot()
-        this.setState({
-            list: store.WeiboHotList,
-            zhihu: store.ZhihuHotList
-        })
+        this.setState(
+            {
+                list: store.WeiboHotList,
+                zhihu: store.ZhihuHotList
+            },
+            () => {
+                console.log(this.state.zhihu)
+            }
+        )
     }
 
     render() {
         return (
             <>
                 <Row>
-                    <Col span={8} className={styles.main_col}>
+                    <Col span={12} className={styles.main_col}>
                         <Card title="weibo Hot" style={cardStl}>
-                            <List dataSource={this.state.list} renderItem={item => <List.Item>{item.content}</List.Item>}></List>
+                            <List dataSource={this.state.list} renderItem={(item: Iitem,i) => <List.Item key={i}>{item?.content}</List.Item>}></List>
                         </Card>
                     </Col>
-                    <Col span={8} className={styles.main_col}>
+                    <Col span={12} className={styles.main_col}>
                         <Card title="zhihu Hot" style={cardStl}>
-                            <List dataSource={this.state.zhihu} renderItem={item => <List.Item>{item.query_display}</List.Item>}></List>
-                        </Card>
-                    </Col>
-                    <Col span={8} className={styles.main_col}>
-                        <Card title="zone Hot" style={cardStl}>
-                            Semi Design 是由互娱社区前端团队与 UED 团队共同设计开发并维护的设计系统。设计系统包含设计语言以及一整套可复用的前端组件，帮助设计师与开发者更容易地打造高质量的、用户体验一致的、符合设计规范的 Web 应用。
+                            <List dataSource={this.state.zhihu} renderItem={(item: Iitem,i) => <List.Item key={i}>{item?.content}</List.Item>}></List>
                         </Card>
                     </Col>
                 </Row>
